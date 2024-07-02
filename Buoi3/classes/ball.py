145ee106ca317d4fe1d9c15d49add27290dd6ef7
Âu Ngọc Diệp <91660142+diepAu13122001@users.noreply.github.com>
2024-07-02 09:40:54 +0700

    pingpong game

--- Buoi3/classes/ball.py
+++ Buoi3/classes/ball.py
@@ -21,13 +21,26 @@ class Ball:
     def getY(self):
         return self.y
 
-    def move(self, paddle_x, paddle_y, paddle_distance):
+    # set image
+    def setImgUrl(self, img_url):
+        self.img_url = img_url
+
+    # get image
+    def getImgUrl(self):
+        return self.img_url
+
+    def move(self, paddle_1_x, paddle_1_y, paddle_1_distance, paddle_2_x, paddle_2_y, paddle_2_distance):
         self.x += self.speed_x
         self.y += self.speed_y
 
         # kiem tra cham paddle
-        if self.x <= (paddle_x + paddle_distance) and paddle_y <= self.y <= (
-            paddle_y + 120
+        if self.x <= (paddle_1_x + paddle_1_distance) and paddle_1_y <= self.y <= (
+            paddle_1_y + 120
+        ):
+            self.speed_x *= -1
+        
+        if self.x <= (paddle_2_x + paddle_2_distance) and paddle_2_y <= self.y <= (
+            paddle_2_y + 120
         ):
             self.speed_x *= -1
 
--- Buoi3/classes/paddle.py
+++ Buoi3/classes/paddle.py
@@ -17,26 +17,11 @@ class Paddle:
     def getY(self):
         return self.y
 
-    # set key press
-    def setKeyUp(self, key_up):
-        self.key_up = key_up
-
-    def setKeyDown(self, key_down):
-        self.key_down = key_down
-
-    # get key press
-    def getKeyUp(self):
-        return self.key_up
-
-    def getKeyDown(self):
-        return self.key_down
-
-    def move(self, key_press):
-        # match - case
-        match key_press:
-            case self.key_up: 
-                if self.y > 0:
-                    self.y -= 5 # move up
-            case self.key_down:
-                if self.y <= 480:
-                    self.y += 5 # move down
+    def move(self, direction, bound_top, bound_down):
+        # neu con trong man choi thi choi tiep
+        if  bound_top <= self.y <= bound_down:
+            self.y += direction * 5
+        elif self.y < bound_top:
+            self.y = bound_top + 1
+        elif self.y > bound_down:
+            self.y = bound_down - 1
